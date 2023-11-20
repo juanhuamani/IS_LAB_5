3ff0819d5380562354d8732f8e65009dcb3c2397
Juan Diego Huamani Vilca <104391377+juanhuamani@users.noreply.github.com>
2023-11-20 11:33:06 -0500

    Update Pruebas.js

--- Pruebas.js
+++ Pruebas.js
@@ -1,4 +1,4 @@
-const calcular_porcentaje = require('./seleniumConfig');
+const calcularPorcentaje = require('./seleniumConfig');
 const QUnit = require('qunit');
 
 class TestCase {
@@ -48,16 +48,16 @@ class TestCase {
 const miCaso = new TestCase();
 
 miCaso.runTest(async function (assert) {
-  const result = await calcularPorcentajePositivo(5, 100);
+  const result = await calcularPorcentaje(5, 100); //POSITIVO
   this.addAssertion(result, "5", 'El resultado debería ser igual a 5');
 });
 
 miCaso.runTest(async function (assert) {
-  const result = await calcularPorcentajeNegativos(-50, -20);
+  const result = await calcularPorcentaje(-50, -20); //NEGATIVO
   this.addAssertion(result, "10", 'El resultado debería ser igual a 10');
 });
 
 miCaso.runTest(async function (assert) {
-  const result = await calcularPorcentajeErroneo(4, 23);
+  const result = await calcularPorcentaje(4, 23); //ERRONEO
   this.addAssertion(result, "3", 'El resultado debería ser igual a 3');
 });
